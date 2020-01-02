5869498671cdef3239b51d95b1d835ddb5c446dd
chiq2045 <charles.oroko@outlook.com>
2020-01-02 21:05:56 +0000

    add logging

--- website.js
+++ website.js
@@ -1,7 +1,16 @@
 const Koa = require('koa');
 const serve = require('koa-static');
+const logger = require('koa-logger');
+const Router = require('@koa/router');
 
 const website = new Koa();
+const home = new Router({
+  prefix: '/'
+});
+const contactAPI = new Router({
+  prefix: 'api/contact'
+});
+website.use(logger());
 
 website.use(serve(`${__dirname}/build`));
 
